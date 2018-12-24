db832edd268130edeb7427694b36edc42dfda9df
WyattLiu <wyatt_liu@hotmail.com>
2018-12-24 17:19:50 -0500

    sensor server

--- simple_webserver/server.pl
+++ simple_webserver/server.pl
@@ -1,15 +1,23 @@
 #!/usr/bin/perl
+use threads;
+our $lock_file = `readlink -f ./camera_api/lock`;
+chomp $lock_file;
+our $usb_control = `readlink -f ../thirdparty/uhubctl/uhubctl`;
+chomp $usb_control;
+
 {
 package PutsWebServer;
  
 use HTTP::Server::Simple::CGI;
 use base qw(HTTP::Server::Simple::CGI);
 
-my $usb_control = `readlink -f ../thirdparty/uhubctl/uhubctl`;
-chomp $usb_control;
+#our $usb_control = `readlink -f ../thirdparty/uhubctl/uhubctl`;
+#chomp $usb_control;
 
+#our $lock_file = `readlink -f ./camera_api/lock`;
+
+#chomp $lock_file;
 print "USB control located at $usb_control \n";
-
 my %dispatch = (
     '/' => \&resp_root,
     '/show_fridge_image' => \&resp_image,
@@ -38,23 +46,26 @@ sub handle_request {
 }
 sub resp_image {
 	#`./camera_api/take_a_photo.pl $usb_control`;
-	my @output = `./show_fridge_image.pl`;
+	my @output = `./show_fridge_image.pl $lock_file`;
 	print @output;
 }	
 sub resp_root {
     
 my $cgi  = shift;   # CGI.pm object
     return if !ref $cgi;
-    `./camera_api/take_a_photo.pl $usb_control`;
+    #`./camera_api/take_a_photo.pl $usb_control`;
 
     my $who = $cgi->param('name');
     
     my $temperature = `cat ../sensor_codes/temp.log | tail -n 1`;
+    my $time_stamp = `ls -lt ./camera_api/latest.jpg | awk '{print \$6,\$7,\$8}'`;
     chomp $temperature;
+    chomp $time_stamp;
     print $cgi->header,
           $cgi->start_html("Root"),
           $cgi->h1("ECE496 Puts projects low level interfaces tester server(production)"),
 	  $cgi->h3("$temperature C");
+	  print "<p>picture is taken at $time_stamp</p>";
 	print "<style>
         	* {
             margin: 0;
@@ -71,12 +82,27 @@ my $cgi  = shift;   # CGI.pm object
         }
     	</style>";
 	  print "<html><body><div class='imgbox'><img class='center-fit 'src=\"show_fridge_image\"></div></body></html>";
+	  
 	  $cgi->end_html;
 }
 
+sub plock {
+	open(my $fh, '>>', $lock_) or die "Cannot lock $lock_\n";
+	my $ret = flock($fh, LOCK_EX);
+	return $fh;
+}
+
+sub punlock {
+	my $fh = shift;
+	close($fh) or die "cannot unlock\n";
+}
 } 
  
 # start the server on port 8080
 my $pid = PutsWebServer->new(8080)->background();
 print "Use 'kill $pid' to stop server.\n";
+print "lock_file is $lock_file\n";
+async {`./camera_api/spin_taking_photo.pl $usb_control $lock_file`};
 #MyWebServer->new(8080);
+
+
--- simple_webserver/show_fridge_image.pl
+++ simple_webserver/show_fridge_image.pl
@@ -1,4 +1,11 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+use Fcntl ':flock';
+
+my $lock = $ARGV[0];
+
+my $lock_h = plock($lock);
 my $file = "camera_api/latest.jpg";
 my $length = -s $file;
 print "Content-type: image/jpg\n";
@@ -8,5 +15,20 @@ open (FH,'<', $file) || die "Could not open $file: $!";
 my $buffer = "";
 while (read(FH, $buffer, 10240)) {
     print $buffer;
+}
+
+	punlock($lock_h);
+
+
+sub plock {
+	my $lock = shift;
+	open(my $fh, '>>', $lock) or die "Cannot lock $lock\n";
+	flock($fh, LOCK_EX) or die "Cannot lock $lock 2\n";
+	return $fh;
+}
+
+sub punlock {
+	my $fh = shift;
+	close($fh) or die "cannot unlock\n";
 }
 
