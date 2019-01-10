23317e661e5c042fb2f1747eac1470c958083222
Z <zheping.jiang@mail.utoronto.ca>
2019-01-10 18:32:16 -0500

    deletion on the remote database enabled. Note: may need to roll back to some previous versions since there always exists build errors for lombok

--- Android App/app/src/main/java/com/example/zhepingjiang/navigation/DisplayItemFragment.java
+++ Android App/app/src/main/java/com/example/zhepingjiang/navigation/DisplayItemFragment.java
@@ -31,6 +31,7 @@ public class DisplayItemFragment extends Fragment {
     LayoutInflater f_inflater;
     ViewGroup f_container;
     private LinearLayout parentLinearLayout;
+    private int num_row = 5;
 
     @Nullable
     @Override
@@ -48,59 +49,110 @@ public class DisplayItemFragment extends Fragment {
         getActivity().setTitle("All items");
 
         final TextView textView = view.findViewById(R.id.displayAllText);
+        final View cur_view = view;
 
-        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        parentLinearLayout = view.findViewById(R.id.display_parent_layout);
-        final View rowView = inflater.inflate(R.layout.field, null);
-        final View rowView2 = inflater.inflate(R.layout.field, null);
-        final View rowView3 = inflater.inflate(R.layout.field, null);
-        parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount());
-        Button rowButton1 = (Button) parentLinearLayout.getChildAt(parentLinearLayout.getChildCount() - 1).findViewById(R.id.delete_button);
-        rowButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "onClick: " + "rowButton1");
-                parentLinearLayout.removeView((View) v.getParent());
-            }
-        });
-        parentLinearLayout.addView(rowView2, parentLinearLayout.getChildCount());
-        parentLinearLayout.addView(rowView3, parentLinearLayout.getChildCount());
-
+        //Request towards the server
         RequestQueue queue = Volley.newRequestQueue(Objects.requireNonNull(getContext()));
-        String url = "http://ece496puts.ddns.net:59496/raw_sql/use putsDB;select std_name from purchase_history;";
+        String url = "http://ece496puts.ddns.net:59496/raw_sql_br/use putsDB;select uid, std_name, expiry_date, status from grocery_storage;";
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                f_dbResult = response;
-                Log.i(TAG, "onResponse: " + f_dbResult);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+            f_dbResult = response;
+            Log.i(TAG, "onResponse: " + f_dbResult);
 
-                Thread t = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (f_dbResult != null) {
-                            f_dbResult = f_dbResult.substring(f_dbResult.indexOf("<h1>") + 4, f_dbResult.indexOf("</h1>"));
-                        }
-
-                        textView.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                textView.setText(f_dbResult);
-                            }
-                        });
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if (f_dbResult != null) {
+                        f_dbResult = f_dbResult.substring(f_dbResult.indexOf("<h1>") + 4, f_dbResult.indexOf("</h1>"));
                     }
-                });
 
-                t.start();
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(f_dbResult);
+                        }
+                    });
+                }
+            });
+
+            t.start();
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("Not working");
+
+            //Create new rows
+            LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            parentLinearLayout = cur_view.findViewById(R.id.display_parent_layout);
+
+            String[][] results =  strParseHelp(f_dbResult);
+            int i;
+            for (String[] result : results) {
+                final View rowView = inflater.inflate(R.layout.field, null);
+                parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount());
+
+                TextView UIDText = (TextView) parentLinearLayout.getChildAt(parentLinearLayout.getChildCount() - 1).findViewById(R.id.uid_text_view);
+                UIDText.setText(result[0]);
+
+                int len = result.length;
+                if (len > 1) {
+                    TextView nameText = (TextView) parentLinearLayout.getChildAt(parentLinearLayout.getChildCount() - 1).findViewById(R.id.name_text_view);
+                    nameText.setText(result[1]);
+                }
+
+                if (len > 2) {
+                    TextView dateText = (TextView) parentLinearLayout.getChildAt(parentLinearLayout.getChildCount() - 1).findViewById(R.id.date_text_view);
+                    dateText.setText(result[2]);
+                }
+
+                if (len > 3) {
+                    TextView statusText = (TextView) parentLinearLayout.getChildAt(parentLinearLayout.getChildCount() - 1).findViewById(R.id.status_text_view);
+                    statusText.setText(result[3]);
+                }
+
+                Button rowButton1 = (Button) parentLinearLayout.getChildAt(parentLinearLayout.getChildCount() - 1).findViewById(R.id.delete_button);
+                    rowButton1.setOnClickListener(v -> {
+                        Log.i(TAG, "onClick: " + "deleteButton: " + v.toString());
+
+                        RequestQueue queue1 = Volley.newRequestQueue(Objects.requireNonNull(getContext()));
+                        String cur_uid = ((TextView)((View)v.getParent()).findViewById(R.id.uid_text_view)).getText().toString();
+                        String url1 = "http://ece496puts.ddns.net:59496/raw_sql_br/use putsDB;delete from grocery_storage where uid=" + cur_uid + ";";
+                        Log.i(TAG, "onClick: " + "url for deletion is: " + url1);
+                        StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url1, new Response.Listener<String>() {
+
+                            @Override
+                            public void onResponse(String response1) {
+                                Log.i(TAG, "onResponse: " + "Deletion successful!\n");
+//                                        textView.setText("Deletion successful\n");
+                            }
+                        }, error -> textView.setText("Deletion failed\n Please check the network"));
+
+                        queue1.add(stringRequest1);
+                        parentLinearLayout.removeView((View) v.getParent());
+                    });
             }
-        });
+        }, error -> textView.setText("No response from the server\n Please check the network"));
 
         queue.add(stringRequest);
+
+//        //Create new rows
+//        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        parentLinearLayout = view.findViewById(R.id.display_parent_layout);
+//
+//        int i = 0;
+//        for (i = 0; i < num_row; i++) {
+//            final View rowView = inflater.inflate(R.layout.field, null);
+//            parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount());
+//            Button rowButton1 = (Button) parentLinearLayout.getChildAt(parentLinearLayout.getChildCount() - 1).findViewById(R.id.delete_button);
+//            rowButton1.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Log.i(TAG, "onClick: " + "rowButton");
+//                    parentLinearLayout.removeView((View) v.getParent());
+//                }
+//            });
+//        }
 
 
 //        handler = new Handler(getContext().getMainLooper());
@@ -122,4 +174,21 @@ public class DisplayItemFragment extends Fragment {
     }
 
 
+    private String[][] strParseHelp(String input) {
+        String[] input_str_line = input.split("<br>");
+
+
+        int input_str_line_size = input_str_line.length;
+        String[][] output = new String[input_str_line_size][];
+
+        int i = 0;
+        for (String input_str : input_str_line) {
+            input_str = input_str.replaceFirst("\\|", "");
+            input_str = input_str.replaceAll(" ", "");
+            output[i] = input_str.split("\\|");
+            i++;
+        }
+
+        return output;
+    }
 }
